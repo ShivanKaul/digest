7ec6586a78b50a96917b9d7389ff3636cbe33702
ShivanKaul <shivan@omsignal.com>
2015-07-25 11:48:12 -0400

    Better project structure, define start script in npm

--- js/server.js
+++ js/server.js
@@ -4,9 +4,9 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+    response.end("Hello World :D\n");
 });
 
 server.listen(process.env.PORT || 8080);
 
-console.log(process.env.PORT);
+console.log("Port currently being used is: " + (process.env.PORT || 8080));
