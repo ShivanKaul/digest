6615ec7e1b4a651fa66a33d305c166ae73e8a728
ShivanKaul <shivan@omsignal.com>
2015-07-25 14:41:13 -0400

    Sending emails, yo. Fix #1, fix #2

--- js/mailsender.js
+++ js/mailsender.js
@@ -1,24 +1,24 @@
-require('dev_resolver')();
-var sendgrid_username   = process.env.SENDGRID_USERNAME;
-var sendgrid_password   = process.env.SENDGRID_PASSWORD;
-var api_key = process.env.SENDGRID_API_KEY;
-var to                  = "yusairamk@hotmail.com";
+function sendEmail (fbResponse) {
+    var sendgrid_username = process.env.SENDGRID_USERNAME
+    var sendgrid_password = process.env.SENDGRID_PASSWORD;
+    var from = "no-reply@fbdigest.com"
+    var to = "shivankaul.1993@gmail.com"
 
-var sendgrid   = require('sendgrid')(api_key);
-var email      = new sendgrid.Email();
+    var sendgrid = require('sendgrid')(sendgrid_username, sendgrid_password);
+    var email = new sendgrid.Email();
 
-email.addTo(to);
-email.setFrom(to);
-email.setSubject('[sendgrid-php-example] Owl');
-email.setText('Owl are you doing?');
-email.setHtml('<strong>%how% are you doing?</strong>');
-email.addSubstitution("%how%", "Owl");
-email.addHeader('X-Sent-Using', 'SendGrid-API');
-email.addHeader('X-Transport', 'web');
+    email.addTo(to);
+    email.setFrom(from);
+    email.setSubject('Test');
+    email.setText(fbResponse);
+    email.addHeader('X-Sent-Using', 'SendGrid-API');
+    email.addHeader('X-Transport', 'web');
 
-
-sendgrid.send(email, function(err, json) {
-  if (err) { return console.error(err); }
-  console.log(json);
-});
-
+    sendgrid.send(email, function (err, json) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(json);
+    });
+}
+module.exports = sendEmail;
--- js/server.js
+++ js/server.js
@@ -1,6 +1,7 @@
 // Load the http module to create an http server.
 var http = require('http');
 var https = require('https');
+var sendEmail = require('./mailsender');
 
 // Note: remember to set FB ACCESS TOKEN as env variable on dev machine.
 var url = "https://graph.facebook.com/v2.4/759985267390294/feed?access_token=" + process.env.ACCESS_TOKEN;
@@ -19,21 +20,10 @@ var server = http.createServer(function (_, responseToSend) {
         });
         responseFB.on('end', function() {
             json = JSON.parse(body);
-            responseToSend.write(JSON.stringify(json, undefined, 2));
+            var fbResponse = JSON.stringify(json, undefined, 2);
+            sendEmail(fbResponse);
+            responseToSend.end("Email sent!");
         });
-        //var results = res.data;
-        //var map = [];
-        //if (results.length > 0) {
-        //    var bound = Math.min(25, results.length); // get either first 25 or however many we got if below 25
-        //    for (var i = 0; i < bound; i++) {
-        //        var tuple = results[i];
-        //        var message = tuple.message;
-        //        var story = tuple.story;
-        //        map.push({message: message, story: story})
-        //
-        //    }
-        //    response._write(map)
-        //}
     }).on('error', function(e) {
         console.log("Got error: " + e.message);
     });
